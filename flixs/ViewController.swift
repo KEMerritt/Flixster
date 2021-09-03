dad6e3975fd37472c7b879da9964baf379a1a6a7
Kitty0567 <kem21@my.fsu.edu>
2021-09-03 16:31:16 -0400

    Initial Commit

---
+++ flixs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixs
+//
+//  Created by Katy Merritt on 9/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
