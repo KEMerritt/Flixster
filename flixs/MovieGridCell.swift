27712553132fa86891d3f00b368b7a3ec76c59e9
Kitty0567 <kem21@my.fsu.edu>
2021-09-12 18:50:17 -0400

    Part 2 of project

---
+++ flixs/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixs
+//
+//  Created by Katy Merritt on 9/10/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
